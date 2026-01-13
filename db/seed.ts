cfd7aba2231d227a0409d5cb70c7fff8edf944ad
Ayad Qaid <ayadqaid12345@gmail.com>
2026-01-13 09:22:11 +0300

    Added webhook for stripe

--- db/seed.ts
+++ db/seed.ts
@@ -1,28 +1,28 @@
-import "dotenv/config";
+// import "dotenv/config";
 
-import sampleData from "./sample-data";
-// import { prisma } from "./prisma-db";
-import { prisma } from "./prisma";
+// import sampleData from "./sample-data";
+// // import { prisma } from "./prisma-db";
+// import { prisma } from "./prisma";
 
-async function main() {
-  await prisma.product.deleteMany();
-  await prisma.account.deleteMany();
-  await prisma.session.deleteMany();
-  await prisma.verificationToken.deleteMany();
-  await prisma.user.deleteMany();
+// async function main() {
+//   await prisma.product.deleteMany();
+//   await prisma.account.deleteMany();
+//   await prisma.session.deleteMany();
+//   await prisma.verificationToken.deleteMany();
+//   await prisma.user.deleteMany();
 
-  await prisma.product.createMany({
-    data: sampleData.products,
-  });
-  await prisma.user.createMany({
-    data: sampleData.users,
-  });
+//   await prisma.product.createMany({
+//     data: sampleData.products,
+//   });
+//   await prisma.user.createMany({
+//     data: sampleData.users,
+//   });
 
-  console.log("Database has been seeded successfully.");
-}
+//   console.log("Database has been seeded successfully.");
+// }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   });
